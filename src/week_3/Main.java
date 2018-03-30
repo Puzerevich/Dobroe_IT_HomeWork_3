1924c036b833a20d11a68ea7bf65db1623bc709d
Puzerevich <puzerevich@gmail.com>
2018-03-31 00:31:20 +0300

    Update Main.java

--- src/week_3/Main.java
+++ src/week_3/Main.java
@@ -1,5 +1,7 @@
 package week_3;
 
+import java.util.Arrays;
+
 /**
  * Created by KOS on 18.03.2018.
  */
@@ -10,53 +12,43 @@ public class Main {
 
 //(Task 1) Create class and subclasses
 
-        Dog dog1 = new Dog();
-        dog1.setAnimal_type("Dog");
-        dog1.setName("Sharik");
-        dog1.setAge(3);
-        System.out.print(dog1.getAnimal_type() + " name: " + dog1.getName() + " \nAge: " + dog1.getAge() + " \n" + dog1.getName() + " say: ");
-        dog1.voice();
+        System.out.println("BEGIN TASK #1");
+        Animal dog1 = new Dog("Dog","Шарик",0.4);
+        Animal dog2 = new Dog("Dog","Гуффи",1.2);
+        Animal dog3 = new Dog("Dog","Мопсик",5.0);
+        Animal dog4 = new Dog("Dog","Жучка",8.2);
+        Animal dog5 = new Dog("Dog","Жирик",9.1);
 
-        System.out.println("- - - - - - - - - - - - - - - - - -");
+        System.out.print(dog1.toString());  dog1.voice();
+        System.out.print(dog2.toString());  dog2.voice();
+        System.out.print(dog3.toString());  dog3.voice();
+        System.out.print(dog4.toString());  dog4.voice();
+        System.out.print(dog5.toString());  dog5.voice();
 
-        Cat cat1 = new Cat();
-        dog1.setAnimal_type("Cat");
-        cat1.setName("Garfield");
-        cat1.setAge(5);
-        System.out.print(cat1.getAnimal_type() + " name: " + cat1.getName() + " \nAge: " + cat1.getAge() + " \n" + cat1.getName() + " say: ");
-        cat1.voice();
+        System.out.println("");
+        Animal cat1 = new Dog("Cat","Аякс",0.7);
+        Animal cat2 = new Dog("Cat","Базилио",1.0);
+        Animal cat3 = new Dog("Cat","Барсик",3.0);
+        Animal cat4 = new Dog("Cat","Пуфик",7.2);
+        Animal cat5 = new Dog("Cat","Феликс",8.1);
 
-        System.out.println("- - - - - - - - - - - - - - - - - -");
-
-        Dog dog2 = new Dog();
-        dog2.setAnimal_type("Dog");
-        dog2.setName("Pushok");
-        dog2.setAge(1);
-        // Один раз вывел просто название класса, вместо вида животного, так как название класса созвучно с видом животного.
-        System.out.print(dog2.getClass().getSimpleName() + " name: " + dog2.getName() + " \nAge: " + dog2.getAge() + " \n" + dog2.getName() + " say: ");
-        dog2.voice();
+        System.out.print(cat1.toString());  cat1.voice();
+        System.out.print(cat2.toString());  cat2.voice();
+        System.out.print(cat3.toString());  cat3.voice();
+        System.out.print(cat4.toString());  cat4.voice();
+        System.out.print(cat5.toString());  cat5.voice();
 
         System.out.println("\nВсего создано объектов: " + cat1.getAnimal_count());
 
-
 //(Task 2) Create array of animal
 
-        Animal[] animal_arr = new Animal[3];
-
-        animal_arr[0] = new Cat();
-        animal_arr[0].setAnimal_type("Cat");
-        animal_arr[0].setName("Cat_1");
-        animal_arr[0].setAge(1.5);
-
-        animal_arr[1] = new Dog();
-        animal_arr[1].setAnimal_type("Dog");
-        animal_arr[1].setName("Degy_1");
-        animal_arr[1].setAge(8);
-
-        animal_arr[2] = new Cat();
-        animal_arr[2].setAnimal_type("Cat");
-        animal_arr[2].setName("Cat_2");
-        animal_arr[2].setAge(11.5);
+        System.out.println("\nBEGIN TASK #2");
+        Animal[] animal_arr = new Animal[5];
+            animal_arr[0] = dog1;
+            animal_arr[1] = dog3;
+            animal_arr[2] = cat1;
+            animal_arr[3] = cat3;
+            animal_arr[4] = cat5;
 
         System.out.println("\nArray of animals:\n");
         for (int i = 0; i < animal_arr.length; i++) {
@@ -66,15 +58,35 @@ public class Main {
         }
 
 //(Task 3) Create array of animal
-        System.out.println("BEGIN TASK #3");
+        System.out.println("\nBEGIN TASK #3");
         animal_arr = (Animal[]) Filter.CatDogGoodbye(animal_arr);
            for (int i = 0; i < animal_arr.length; i++) {
               if (animal_arr[i] != null)
                System.out.println("Animal type: " + animal_arr[i].getAnimal_type() + "Name: " + animal_arr[i].getName() + " Age: " + animal_arr[i].getAge() + " " + animal_arr[i].getName() + " say: ");
          }
-        System.out.println("END TASK #3");
+
+
+//(Task 4) Pitomnik2
+
+        System.out.println("\nBEGIN TASK #4");
+        Pitomnik shelter = new Pitomnik();
+
+        shelter.addAnimals(cat1);
+        shelter.addAnimals(cat2);
+        shelter.addAnimals(cat3);
+        shelter.addAnimals(cat5);
+
+        shelter.addAnimals(dog1);
+        shelter.addAnimals(dog2);
+        shelter.addAnimals(dog4);
+        shelter.addAnimals(dog5);
+
+        shelter.printAnimals(Cat.class.getSimpleName(), 0, 8);
+
+ // TASK 5
+        System.out.println("\nBEGIN TASK #5");
+        int[] inputArray ={5,3,7,3,1,9,2,9,0,4,9};
+        System.out.println(Arrays.toString(Filter.returnArr(inputArray)));
 
     }
 }
-
-
