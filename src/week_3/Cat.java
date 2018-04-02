e2c7af89c0e0c8ee341030674d8dcdd680367754
Puzerevich <puzerevich@gmail.com>
2018-04-03 01:07:23 +0300

    Update Cat.java

--- src/week_3/Cat.java
+++ src/week_3/Cat.java
@@ -1,9 +1,16 @@
 package week_3;
 
 /**
- * Created by Konstantin on 20.03.2018.
+ * Created by Konstantin on 20.03.2018. WITH CORRECT
  */
 public class Cat extends Animal{
+
+    public Cat() {
+    }
+
+    public Cat(String a_name, double a_age) {
+        super(a_name, a_age);
+    }
 
     @Override
     void voice() {
