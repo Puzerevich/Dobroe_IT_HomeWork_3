c490ac9c9fc9fd9b9e3d82750579506ff12a7604
Puzerevich <puzerevich@gmail.com>
2018-04-03 01:07:46 +0300

    Update Dog.java

--- src/week_3/Dog.java
+++ src/week_3/Dog.java
@@ -1,12 +1,18 @@
 package week_3;
 
 /**
- * Created by Konstantin on 20.03.2018.
+ * Created by Konstantin on 20.03.2018. WITH CORRECT
  */
 public class Dog extends Animal{
 
+    public Dog() {
+    }
 
-        @Override
+    public Dog(String a_name, double a_age) {
+        super(a_name, a_age);
+    }
+
+    @Override
         void voice() {
             System.out.println("Гав");
         }
