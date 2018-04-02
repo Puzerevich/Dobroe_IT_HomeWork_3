923a6241f01e582b36ca7f418b1fb0f9c1cfd7fd
Puzerevich <puzerevich@gmail.com>
2018-04-03 01:08:34 +0300

    Update Animal.java

--- src/week_3/Animal.java
+++ src/week_3/Animal.java
@@ -1,11 +1,10 @@
 package week_3;
 
 /**
- * Created by Konstantin on 20.03.2018.
+ * Created by Konstantin on 20.03.2018.  WITH CORRECT
  */
 public class Animal {
-
-    private String animal_type;
+ 
     private String name;
     private double age;
     private static int animal_count;
@@ -14,23 +13,15 @@ public class Animal {
         animal_count++;
     }
 
-    public Animal(  String a_type, String a_name,  double a_age) {
-        this.animal_type = a_type;
+    public Animal(  String a_name,  double a_age) {
         this.name = a_name;
         this.age = a_age;
-    }
 
-    public String getAnimal_type() {
-        return animal_type;
-    }
-
-    public void setAnimal_type(String animal_type) {
-        this.animal_type = animal_type;
     }
 
     public int getAnimal_count() {
-        return animal_count;
-    }
+         return animal_count;
+     }
 
     public String getName() {
         return name;
@@ -46,6 +37,15 @@ public class Animal {
 
     public void setAge(double age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+               "animal_type='" + getClass().getSimpleName() + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 
     void voice(){}
