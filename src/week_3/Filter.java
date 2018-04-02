29bc4f8cc5ad88dd4c9c8cae9bb553cb853b799b
Puzerevich <puzerevich@gmail.com>
2018-04-03 01:09:06 +0300

    Update Filter.java

--- src/week_3/Filter.java
+++ src/week_3/Filter.java
@@ -3,7 +3,7 @@ package week_3;
 import java.util.Arrays;
 
 /**
- * Created by Konstantin on 30.03.2018.
+ * Created by Konstantin on 30.03.2018.  WITH CORRECT
  */
 public class Filter {
 
@@ -24,14 +24,13 @@ public class Filter {
 
     public static Object CatDogGoodbye(Animal an_arr[]){
         int nElems = an_arr.length;
-        Animal[] tmpArr = new Animal[nElems];
         for (int i = 0; i < nElems; i++) {
             String name = an_arr[i].getClass().getSimpleName();
             if (name.equals("Cat") && (an_arr[i].getAge() < 1.0 || an_arr[i].getAge() > 8.0)) {
                 an_arr[i]=null;
             }
             else{
-                if (name.equals("Dog")&& FilterName(an_arr[i].getName())<4) {
+                if (name.equals("Dog")&& FilterName(an_arr[i].getName())>4) {
                     an_arr[i]=null;
                 }
             }
